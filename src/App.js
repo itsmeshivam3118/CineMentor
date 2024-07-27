071ea49631766a52fb413f5b0e527716e660dd0f
itsmeshivam3118 <itsmeshivam3118@gmail.com>
2024-07-27 11:26:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="text-3xl text-yellow-700">hello</header>
+      <header className="text-3xl text-yellow-700">hello Guys</header>
     </div>
   );
 }
